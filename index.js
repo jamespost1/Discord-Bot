404fa1abb865c57494565ab46831e58b5c781f9e
James Post <jamesbrendanpost@gmail.com>
2022-10-17 21:45:52 -0400

    updated discord bot replies

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js")
 
-const TOKEN = "MTAyNjUzNTA4MzE3NzQxNDc1Ng.GAH_1j.CndddAt96ZYNH4LSM_wGK40wLq0aLKyIb5vgFc"
+const TOKEN = "MTAyNjUzNTA4MzE3NzQxNDc1Ng.GQeUed.axkV66SAmoklnvE5rxnBSMjBD9dvz2RoufCT1c"
 
+const keep_alive = require('./keep_alive.js')
 
 const client = new Discord.Client({
     intents: [
@@ -11,14 +12,14 @@ const client = new Discord.Client({
     ]
 })
 
-client.on("ready", () =>{
-    console.log('Logged in!')
+client.on('ready', () =>{
+    console.log("Logged in!")
 })
 
-client.on("message.Create", (message) =>{
-    if(message.content == "Hi Bot!") {
-        message.reply("Hello! It's me!")
+client.on('message', msg => {
+    if (msg.content === 'hello' || msg.content === 'hey' || msg.content === 'hi') {
+      msg.reply('hello friend');
     }
-})
+    })
 
 client.login(TOKEN)
